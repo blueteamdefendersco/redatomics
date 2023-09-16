99b05ba13e7e173aa448c95f5423f2b8f894b3ed
blueteamdefendersco <145217566+blueteamdefendersco@users.noreply.github.com>
2023-09-16 13:32:29 -0500

    Update T1059_004-1.sh

--- atomic_scripts/linux/execution/T1059_004-1.sh
+++ atomic_scripts/linux/execution/T1059_004-1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sh -c "echo 'echo Created and executed a simple bash script' > /tmp/art.sh
-sh -c "echo 'ping -c 4 8.8.8.8' >> /tmp/art.sh
+bash -c "echo 'echo Created and executed a simple bash script' > /tmp/art.sh
+bash -c "echo 'ping -c 4 8.8.8.8' >> /tmp/art.sh
 chmod +x /tmp/art.sh
-sh /tmp/art.sh
+bash /tmp/art.sh
 
 sleep 30
 
