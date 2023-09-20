8cea35c6bf20c82c4aabb01f6cfcc96b709cbb62
blueteamdefendersco <145217566+blueteamdefendersco@users.noreply.github.com>
2023-09-20 10:18:55 -0500

    Create T1053_002-2.sh

---
+++ atomic_scripts/linux/execution/T1053_002-2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This test submits a command to be run in the future by the at daemon
+
+bash -c echo "================ Atomic Test - At - Schedule a Job ==============="
+
+bash -c echo "echo Hello from Atomic Red Team" > /tmp/testing_T1053_002-2.txt | at now + 1 minute
+
+sleep 180
+
+rm /tmp/testing_T1053_002-2.txt
