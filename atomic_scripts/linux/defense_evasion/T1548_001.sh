1d060d9fa718ca5d26126015661a4036592441a6
blueteamdefendersco <145217566+blueteamdefendersco@users.noreply.github.com>
2023-09-24 19:05:45 -0500

    Update T1548_001.sh

--- atomic_scripts/linux/defense_evasion/T1548_001.sh
+++ atomic_scripts/linux/defense_evasion/T1548_001.sh
@@ -10,6 +10,7 @@ function a() {
     sudo chown root /tmp/hello
     sudo chmod u+s /tmp/hello
     /tmp/hello
+    echo  $(date -u) "Completed T1548.001 - Make and modify binary from C source" >> /tmp/attacktest.txt 
     sleep 60
 }
 
@@ -19,6 +20,7 @@ function b() {
     make /tmp/hello2
     chmod u+s /tmp/hello2
     /tmp/hello2
+    echo  $(date -u) "Completed T1548.001 - Make and modify binary from C source" >> /tmp/attacktest.txt
     sleep 60
 }
 
@@ -26,6 +28,7 @@ function c() {
     # Atomic Test 2 - Set a SetUID flag on file
     touch /tmp/evilBinary
     chmod u+xs /tmp/evilBinary
+    echo  $(date -u) "Completed T1548.001 - Set a SetUID flag on file" >> /tmp/attacktest.txt
     sleep 60
 }
 
@@ -34,6 +37,7 @@ function d() {
     sudo touch /tmp/evilBinary2
     sudo chown root /tmp/evilBinary2
     sudo chmod u+xs /tmp/evilBinary2
+    echo  $(date -u) "Completed T1548.001 - Set a SetUID flag on file" >> /tmp/attacktest.txt
     sleep 60
 }
 
@@ -42,6 +46,7 @@ function e() {
     sudo touch /tmp/evilBinary3
     sudo chown root /tmp/evilBinary3
     sudo chmod g+xs /tmp/evilBinary3
+    echo  $(date -u) "Completed T1548.001 - Set a SetGID flag on file" >> /tmp/attacktest.txt
     sleep 60
 }
 
@@ -49,6 +54,7 @@ function f() {
     # Atomic Test 3 - Set a SetGID flag on file
     touch /tmp/evilBinary4
     chmod g+xs /tmp/evilBinary4
+    echo  $(date -u) "Completed T1548.001 - Set a SetGID flag on file" >> /tmp/attacktest.txt
     sleep 60
 }
 
@@ -59,6 +65,7 @@ function g() {
     make /tmp/cap
     sudo setcap cap_setuid=ep /tmp/cap
     /tmp/cap
+    echo  $(date -u) "Completed T1548.001 - Make and modify capabilities of a binary" >> /tmp/attacktest.txt
     sleep 60
 }
 
@@ -66,18 +73,21 @@ function h() {
     # Atomic Test 5 - Provide the SetUID capability to a file
     touch /tmp/evilBinary5
     sudo setcap cap_setuid=ep /tmp/evilBinary5
+    echo  $(date -u) "Completed T1548.001 - Provide the SetUID capability to a file" >> /tmp/attacktest.txt
     sleep 60
 }
 
 function i() {
 # Atomic Test 6 - Do reconnaissance for files that have the setuid bit set
     find /usr/bin -perm -4000
+    echo  $(date -u) "Completed T1548.001 - Do reconnaissance for files that have the setuid bit set" >> /tmp/attacktest.txt
     sleep 60
 }
 
 function j() {
 # Atomic Test 7 - Do reconnaissance for files that have the setgid bit set
     find /usr/bin -perm -2000
+    echo  $(date -u) "Completed T1548.001 - Do reconnaissance for files that have the setgid bit set" >> /tmp/attacktest.txt
     sleep 60
 }
 
@@ -95,6 +105,9 @@ function l() {
 
 function z() {
     # Clean up everything
+    sudo rm /tmp/hello.c
+    sudo rm /tmp/hello2.c
+    sudo rm /tmp/cap.c
     sudo rm /tmp/hello
     rm /tmp/hello2
     rm /tmp/evilBinary
@@ -105,6 +118,7 @@ function z() {
     sudo rm /tmp/evilBinary5
     rm /tmp/evilBinary6
     rm /tmp/evilBinary7
+    echo  $(date -u) "Cleaned up T1548.001" >> /tmp/attacktest.txt
     sleep 60
 }
 
